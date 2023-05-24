e5d44dc050d064b9a19bbdee357cb154f798bbc0
tsb-uy-hw0040 <tsb-uy-hw0040@192.168.1.4>
2023-05-24 17:12:53 -0300

    Add remotes a and b

---
+++ remote-A/nuxt.config.ts
@@ -0,0 +1,45 @@
+
+import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
+import topLevelAwait from 'vite-plugin-top-level-await'
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  vite: {
+    plugins: [
+      vue(),
+      federation({
+        name: 'home',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './Button': './src/components/Button.vue',
+        },
+        shared: {
+          vue: {},
+          pinia: {},
+          myStore: {
+            packagePath: './src/store.js',
+            import: false,
+            generate: false,
+          },
+        },
+      }),
+      topLevelAwait({
+        // The export name of top-level await promise for each chunk module
+        promiseExportName: '__tla',
+        // The function to generate import names of top-level await promise in each chunk module
+        promiseImportName: (i) => `__tla_${i}`,
+      }),
+    ],
+    build: {
+      assetsInlineLimit: 40960,
+      minify: true,
+      cssCodeSplit: false,
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          minifyInternalExports: false,
+        },
+      },
+    },
+  },
+});
